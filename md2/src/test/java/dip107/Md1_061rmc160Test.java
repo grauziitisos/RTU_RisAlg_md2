f56231d6962ae44e6859f7e19a1ce9f40581434a
Oskars Grauzis <oskars_grauzis@inbox.lv>
2021-09-06 20:45:45 +0300

    junit moved to jupiter

--- md2/src/test/java/dip107/Md1_061rmc160Test.java
+++ md2/src/test/java/dip107/Md1_061rmc160Test.java
@@ -6,7 +6,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
